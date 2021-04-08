e651bbdb55a0aa2c47d3f1969caca2e5a6bf2e78
cryst <cryst@192.168.219.188>
2021-04-09 07:16:08 +0900

    데이터 소스 문제로 실행이 안됨 수정 필요.

---
+++ leverage-parent/leverageapart-api/src/main/java/com/hakoohakoo/leverageapart/web/SpringWebApplication.java
@@ -0,0 +1,12 @@
+package com.hakoohakoo.leverageapart.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplication {
+	public static void main(String[] args) {
+		System.setProperty("sun.net.http.allowRestrictedHeaders", "true");
+        SpringApplication.run(SpringWebApplication.class, args);
+    }
+}
