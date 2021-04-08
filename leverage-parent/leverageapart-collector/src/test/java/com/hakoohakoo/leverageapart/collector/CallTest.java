8bce9958f4257ad3adea9cef52a2edac361fbd2d
cryst <cryst@192.168.219.188>
2021-04-08 23:54:48 +0900

    연동 테스트 완료

--- leverage-parent/leverageapart-collector/src/test/java/com/hakoohakoo/leverageapart/collector/CallTest.java
+++ leverage-parent/leverageapart-collector/src/test/java/com/hakoohakoo/leverageapart/collector/CallTest.java
@@ -5,9 +5,13 @@ import java.time.Duration;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.util.MimeType;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hakoohakoo.leverageapart.rawdata.KaptDetailResponse;
 
 import lombok.extern.slf4j.Slf4j;
@@ -18,21 +22,25 @@ import reactor.test.StepVerifier;
 @Slf4j
 public class CallTest {
 	@Test
-	public void kaptCall() {
-		Flux<KaptDetailResponse> response = Flux.just("A10024501","A13583301","A13582002","1168010800").delayElements(Duration.ofSeconds(1)).map(aptCode -> {
-			return WebClient.create("http://www.k-apt.go.kr").post()
+	public void kaptCall() throws Exception {	
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		Jackson2JsonDecoder decoder = new Jackson2JsonDecoder(mapper, new MimeType("text", "plain", StandardCharsets.UTF_8), new MimeType("text", "html", StandardCharsets.UTF_8));
+		
+		Flux<KaptDetailResponse> response = Flux.just("A10024501","A13583301","A13582002").delayElements(Duration.ofSeconds(1)).map(aptCode -> {
+			return WebClient.builder().codecs(clientCodecConfigurer -> clientCodecConfigurer.customCodecs().register(decoder)).baseUrl("http://www.k-apt.go.kr").build().post()
 				.uri(uriBuilder -> uriBuilder.path("/kaptinfo/getKaptInfo_detail.do").build())
 				.contentType(new MediaType("text", "html", StandardCharsets.UTF_8))
-				.accept(MediaType.APPLICATION_JSON)
+				.accept(new MediaType("text", "html", StandardCharsets.UTF_8))
 				.body(BodyInserters.fromFormData("kapt_code", aptCode))
 				.retrieve()
-				.bodyToFlux(KaptDetailResponse.class);
-			}).blockLast();
+				.bodyToFlux(KaptDetailResponse.class).log();
+			}).log().blockLast();
 		
 		
 		StepVerifier.create(response)
-			//.expectSubscription()
-			.consumeNextWith(res -> log.debug("아파트 이름 : {}", res.getDetail().getName()))
+			.expectSubscription()
+			.consumeNextWith(res -> log.debug("res : {}", res))
 			.verifyComplete();
 	}
 }
